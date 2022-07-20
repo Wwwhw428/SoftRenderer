130f6fd7dd66c20053bdeae44e49bd074ff1d8bd
Wwwhw428 <wanghaowen28@gmail.com>
2022-07-20 08:58:00 +0800

    draw line

--- main.cpp
+++ main.cpp
@@ -1,35 +1,28 @@
-//#include <GLFW/glfw3.h>
-//#include <iostream>
-//#include <windows.h>
-//
-//int main()
-//{
-//	if (!glfwInit())
-//	{
-//		// Initialization failed
-//		std::cout << "init failed" << std::endl;
-//	}
-//
-//	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
-//	if (!window)
-//	{
-//		// Window or OpenGL context creation failed
-//		std::cout << "windows creation failed" << std::endl;
-//
-//	}
-//
-//	Sleep(5000);
-//
-//	return 0;
-//}
-
+#include <iostream>
 #include "tgaimage.h"
+
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+
+TGAImage drawLine(int x0, int y0, int x1, int y1, TGAImage image, TGAColor color)
+{
+	for (float t = 0; t < 1.0; t += 0.01)
+	{
+		int x = x0 + (x1 - x0) * t;
+		int y = y0 + (y1 - y0) * t;
+		image.set(x, y, color);
+	}
+
+	return image;
+}
+
+
 int main(int argc, char** argv) {
-    TGAImage image(100, 100, TGAImage::RGB);
-    image.set(52, 41, red);
-    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-    image.write_tga_file("output.tga");
-    return 0;
+	TGAImage image(100, 100, TGAImage::RGB);
+	//image.set(52, 41, red);
+
+	image = drawLine(0, 0, 15, 30, image, red);
+	//image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	image.write_tga_file("output.tga");
+	return 0;
 }
