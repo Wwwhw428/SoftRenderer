58a4b9bf35a4d1148ada82796c88e672780c18a7
Wwwhw428 <wanghaowen28@gmail.com>
2022-07-27 19:20:57 +0800

    init again

--- main.cpp
+++ main.cpp
@@ -1,28 +1,23 @@
-#include <iostream>
-#include "tgaimage.h"
+#include <opencv2/opencv.hpp>
 
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red = TGAColor(255, 0, 0, 255);
+using namespace std;
+using namespace cv;
 
-TGAImage drawLine(int x0, int y0, int x1, int y1, TGAImage image, TGAColor color)
-{
-	for (float t = 0; t < 1.0; t += 0.01)
-	{
-		int x = x0 + (x1 - x0) * t;
-		int y = y0 + (y1 - y0) * t;
-		image.set(x, y, color);
-	}
+int main(int argc, char* argv[]) {
+    const char* imagename = "F:/blender-data/WL0036/normal_reduce.png";//此处为你自己的图片路径
 
-	return image;
-}
+    //从文件中读入图像
+    Mat img = imread(imagename, 1);
 
+    //如果读入图像失败
+    if (img.empty()) {
+        fprintf(stderr, "Can not load image %s\n", imagename);
+        return -1;
+    }
+    //显示图像
+    imshow("image", img);
 
-int main(int argc, char** argv) {
-	TGAImage image(100, 100, TGAImage::RGB);
-	//image.set(52, 41, red);
-
-	image = drawLine(0, 0, 15, 30, image, red);
-	//image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-	image.write_tga_file("output.tga");
-	return 0;
+    //此函数等待按键，按键盘任意键就返回
+    waitKey();
+    return 0;
 }
